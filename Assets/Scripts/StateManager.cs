b98a715dee13e28f83f098654a917f6c4525470f
Felipe Pavanelli <felipe.g.pavanelli.26@dartmouth.edu>
2024-07-09 17:19:19 -0400

    improved joystick rotation controls

---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    public GameObject block;
+    public float rotateSpeed = 10f;
+    public float smoothTime = 0.1f;
+    
+    private float targetRotationY;
+    private float currentRotationY;
+    private float rotationVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRotationY = block.transform.eulerAngles.y;
+        currentRotationY = targetRotationY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal_Primary");
+        if (horizontalInput != 0)
+        {
+            targetRotationY += horizontalInput * rotateSpeed;
+        }
+
+        currentRotationY = Mathf.SmoothDampAngle(currentRotationY, targetRotationY, ref rotationVelocity, smoothTime);
+        block.transform.eulerAngles = new Vector3(block.transform.eulerAngles.x, currentRotationY, block.transform.eulerAngles.z);
+    }
+}
